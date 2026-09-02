ba11a020a0e33da10fe3aa036e5693bab9a2951c
kongsni <binybinybiny03@gmail.com>
2026-09-02 16:34:16 +0900

    feat: initial commit with spring boot

---
+++ src/main/java/com/tradevault/client/SecClient.java
@@ -0,0 +1,4 @@
+package com.tradevault.client;
+
+public class SecClient {
+}
---
+++ src/test/java/com/tradevault/TradeVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tradevault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeVaultApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
